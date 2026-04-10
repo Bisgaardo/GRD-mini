774b7c5cca5672ce9da832c9963ddf21dee47c1a
Goodest person <mflens22@student.aau.dk>
2026-04-10 14:29:05 +0200

    reworked inventory

--- Assets/Scripts/Backpack menu.cs
+++ Assets/Scripts/Backpack menu.cs
@@ -1,57 +1,21 @@
-using System;
-using System.Net.Mime;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class Backpackmenu : MonoBehaviour, IDropHandler
+public class Slot : MonoBehaviour
 {
-    private Item currentitem;
+    private Item currentItem;
 
-    public bool isThrow;
-    
-    private void Awake()
+    public bool IsOccupied()
     {
+        return currentItem != null;
     }
 
-    public void OnDrop(PointerEventData eventData)
+    public void OccupySlot(Item item)
     {
-        if (eventData.pointerDrag != null)
-        {
-            Item draggeditem = eventData.pointerDrag.GetComponent<Item>(); //gets the dragged item
-            if (draggeditem == null)
-            {
-                return; //stops if loop 
-            }
-
-            if (currentitem != null)
-            {
-                Debug.Log("occupied!");
-                return;
-            }
-            Debug.Log("Drop");
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition =
-                GetComponent<RectTransform>().anchoredPosition;
-            currentitem = draggeditem;
-            draggeditem.SetSlot(this);
-        }
-
-        if (isThrow)
-        {
-            Destroy(currentitem.gameObject);
-        }
-
+        currentItem = item;
     }
+
     public void ClearSlot()
     {
-        Debug.Log("clear slot");
-        currentitem = null;
-    }
-    
-    public void PlaceItem(Item item)
-    {
-        currentitem = item;
+        currentItem = null;
     }
 }
-
